a213b51021a2ff75d4f64f30cf67947f298938c1
Milica Jovanovic <j.milica95@yahoo.com>
2017-02-07 10:53:54 +0100

    Nova verzija baze podataka

--- obracunplata.sql
+++ obracunplata.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 03, 2017 at 01:36 PM
+-- Generation Time: Feb 07, 2017 at 10:48 AM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -28,27 +28,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `employee` (
   `employee_id` int(11) NOT NULL,
-  `semester_number` int(11) NOT NULL,
-  `subjects_in_spring_semester` int(11) NOT NULL,
-  `subjects_in_autumn_semester` int(11) NOT NULL,
-  `special_add_value` int(11) NOT NULL,
-  `functionas_add_value` int(11) NOT NULL,
+  `semester_number` int(11) NOT NULL DEFAULT '0',
+  `subjects_in_spring_semester` int(11) NOT NULL DEFAULT '0',
+  `subjects_in_autumn_semester` int(11) NOT NULL DEFAULT '0',
+  `special_add_value` int(11) NOT NULL DEFAULT '0',
+  `functions_add_value` int(11) NOT NULL DEFAULT '0',
   `name` varchar(250) COLLATE utf8_bin NOT NULL,
   `lastname` varchar(250) COLLATE utf8_bin NOT NULL,
   `faculty` varchar(250) COLLATE utf8_bin NOT NULL,
   `bank_account` varchar(250) COLLATE utf8_bin NOT NULL,
   `email` varchar(250) COLLATE utf8_bin NOT NULL,
   `teaching_position` varchar(250) COLLATE utf8_bin NOT NULL,
-  `eployment_percentage` varchar(250) COLLATE utf8_bin NOT NULL,
+  `employment_percentage` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT '',
   `subject_number` varchar(250) COLLATE utf8_bin NOT NULL,
   `kbp` varchar(250) COLLATE utf8_bin NOT NULL,
   `kro` varchar(250) COLLATE utf8_bin NOT NULL,
   `kt` varchar(250) COLLATE utf8_bin NOT NULL,
   `kpr` varchar(250) COLLATE utf8_bin NOT NULL,
-  `isum_hours_autumn` float NOT NULL,
-  `isum_hours_spring` float NOT NULL,
-  `isum_money_autumn` float NOT NULL,
-  `isum_money_spring` float NOT NULL
+  `isum_hours_autumn` float NOT NULL DEFAULT '0',
+  `isum_hours_spring` float NOT NULL DEFAULT '0',
+  `isum_money_autumn` float NOT NULL DEFAULT '0',
+  `isum_money_spring` float NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -58,22 +58,24 @@ CREATE TABLE `employee` (
 --
 
 CREATE TABLE `mdita` (
-  `id` int(11) NOT NULL,
-  `broj_reci` float NOT NULL,
-  `broj_objekata_ucenja` float NOT NULL,
-  `assesment` float NOT NULL,
-  `multiple_choice` float NOT NULL,
-  `question_and_answers` float NOT NULL,
-  `java_grader` float NOT NULL,
-  `forum` float NOT NULL,
-  `noticeboard` float NOT NULL,
-  `notebook` float NOT NULL,
-  `chat` float NOT NULL,
-  `submit_files` float NOT NULL,
-  `shared_resources` float NOT NULL,
-  `picture_gallery` float NOT NULL,
-  `video` float NOT NULL,
-  `audio` float NOT NULL
+  `mdita_id` int(11) NOT NULL,
+  `broj_reci` float NOT NULL DEFAULT '0',
+  `broj_objekata_ucenja` float NOT NULL DEFAULT '0',
+  `assesment` float NOT NULL DEFAULT '0',
+  `multiple_choice` float NOT NULL DEFAULT '0',
+  `question_and_answers` float NOT NULL DEFAULT '0',
+  `java_grader` float NOT NULL DEFAULT '0',
+  `forum` float NOT NULL DEFAULT '0',
+  `noticeboard` float NOT NULL DEFAULT '0',
+  `notebook` float NOT NULL DEFAULT '0',
+  `chat` float NOT NULL DEFAULT '0',
+  `submit_files` float NOT NULL DEFAULT '0',
+  `shared_resources` float NOT NULL DEFAULT '0',
+  `picture_gallery` float NOT NULL DEFAULT '0',
+  `video` float NOT NULL DEFAULT '0',
+  `audio` float NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL DEFAULT '0',
+  `mditaId` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -115,66 +117,77 @@ CREATE TABLE `subject` (
   `individual_exercise_number` int(11) NOT NULL,
   `espb` int(11) NOT NULL,
   `groups_number` int(11) NOT NULL,
-  `change_percetnage` float NOT NULL,
-  `proc1` float NOT NULL,
-  `proc2` float NOT NULL,
-  `proc3` float NOT NULL,
-  `proc4` float NOT NULL,
-  `proc5` float NOT NULL,
-  `proc6` float NOT NULL,
-  `proc7` float NOT NULL,
-  `proc8` float NOT NULL,
-  `proc9` float NOT NULL,
-  `proc10` float NOT NULL,
-  `proc11` float NOT NULL,
-  `proc12` float NOT NULL,
-  `proc13` float NOT NULL,
-  `proc14` float NOT NULL,
-  `proc15` float NOT NULL,
   `words_number` float NOT NULL,
-  `fee` float NOT NULL,
   `fpm` float NOT NULL,
   `fob` float NOT NULL,
   `fin1` float NOT NULL,
   `fin2` float NOT NULL,
   `fmm` float NOT NULL,
-  `fkv` float NOT NULL,
+  `fkv` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_employee`
+--
+
+CREATE TABLE `subject_employee` (
+  `subject_employee_id` int(11) UNSIGNED NOT NULL,
   `employee_id` int(11) NOT NULL,
-  `part_time_employee_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `subject_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subject_mark`
+-- Table structure for table `subject_part_time_employee`
 --
 
-CREATE TABLE `subject_mark` (
-  `subject_mark_id` int(11) NOT NULL,
-  `name` varchar(250) COLLATE utf8_bin NOT NULL,
-  `value` float NOT NULL,
+CREATE TABLE `subject_part_time_employee` (
+  `subject_part_time_employee_id` int(11) UNSIGNED NOT NULL,
+  `part_time_employee_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
   `username` varchar(250) COLLATE utf8_bin NOT NULL,
-  `role` varchar(250) COLLATE utf8_bin NOT NULL,
-  `password` varchar(250) COLLATE utf8_bin NOT NULL
+  `password` varchar(250) COLLATE utf8_bin NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `user` (`id`, `username`, `role`, `password`) VALUES
-(1, 'admin', 'admin', 'admin');
+INSERT INTO `users` (`user_id`, `username`, `password`, `enabled`) VALUES
+(1, 'admin', 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_role_id` int(11) NOT NULL,
+  `username` varchar(250) COLLATE utf8_bin NOT NULL,
+  `role` varchar(25) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_role_id`, `username`, `role`) VALUES
+(1, 'admin', 'ROLE_ADMIN');
 
 --
 -- Indexes for dumped tables
@@ -190,7 +203,7 @@ ALTER TABLE `employee`
 -- Indexes for table `mdita`
 --
 ALTER TABLE `mdita`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`mdita_id`);
 
 --
 -- Indexes for table `part_time_employee`
@@ -202,22 +215,35 @@ ALTER TABLE `part_time_employee`
 -- Indexes for table `subject`
 --
 ALTER TABLE `subject`
-  ADD PRIMARY KEY (`subject_id`),
-  ADD KEY `FK_706i7tusxpuvh2ks0vd1o91df` (`employee_id`),
-  ADD KEY `FK_9annjlxsgi2uucuke8jw0e05f` (`part_time_employee_id`);
+  ADD PRIMARY KEY (`subject_id`);
 
 --
--- Indexes for table `subject_mark`
+-- Indexes for table `subject_employee`
 --
-ALTER TABLE `subject_mark`
-  ADD PRIMARY KEY (`subject_mark_id`),
-  ADD KEY `FK_rnj4xhbv5n0jx2qh140y1b713` (`subject_id`);
+ALTER TABLE `subject_employee`
+  ADD PRIMARY KEY (`subject_employee_id`),
+  ADD KEY `se_employee` (`employee_id`),
+  ADD KEY `se_subject` (`subject_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `subject_part_time_employee`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `subject_part_time_employee`
+  ADD PRIMARY KEY (`subject_part_time_employee_id`),
+  ADD KEY `sp_employee` (`part_time_employee_id`),
+  ADD KEY `sp_subject` (`subject_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_role_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -227,48 +253,59 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `mdita`
 --
 ALTER TABLE `mdita`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `mdita_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `part_time_employee`
 --
 ALTER TABLE `part_time_employee`
-  MODIFY `part_time_employee_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `part_time_employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 --
--- AUTO_INCREMENT for table `subject_mark`
+-- AUTO_INCREMENT for table `subject_employee`
 --
-ALTER TABLE `subject_mark`
-  MODIFY `subject_mark_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `subject_employee`
+  MODIFY `subject_employee_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `subject_part_time_employee`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `subject_part_time_employee`
+  MODIFY `subject_part_time_employee_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `user_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `subject`
+-- Constraints for table `subject_employee`
 --
-ALTER TABLE `subject`
-  ADD CONSTRAINT `FK_706i7tusxpuvh2ks0vd1o91df` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`),
-  ADD CONSTRAINT `FK_9annjlxsgi2uucuke8jw0e05f` FOREIGN KEY (`part_time_employee_id`) REFERENCES `part_time_employee` (`part_time_employee_id`);
+ALTER TABLE `subject_employee`
+  ADD CONSTRAINT `se_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`),
+  ADD CONSTRAINT `se_subject` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
 
 --
--- Constraints for table `subject_mark`
+-- Constraints for table `subject_part_time_employee`
 --
-ALTER TABLE `subject_mark`
-  ADD CONSTRAINT `FK_rnj4xhbv5n0jx2qh140y1b713` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
+ALTER TABLE `subject_part_time_employee`
+  ADD CONSTRAINT `sp_employee` FOREIGN KEY (`part_time_employee_id`) REFERENCES `part_time_employee` (`part_time_employee_id`),
+  ADD CONSTRAINT `sp_subject` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
